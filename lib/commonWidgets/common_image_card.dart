902b3d00fab715e3395ab22b2b2d171b911f9e1f
ivansiuzzzzz <ivan.siu@icloud.com>
2023-07-15 12:29:34 +0800

    remove no usage widget

--- lib/commonWidgets/common_image_card.dart
+++ lib/commonWidgets/common_image_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:mealsapp/commonWidgets/item_trait.dart';
+import 'package:shoppingshop/commonWidgets/item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class CommonImageCardWidget extends StatelessWidget {
